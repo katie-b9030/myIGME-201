a5b95cebf63f7fb56bfade1dac2e3b2ade68395b
Katie Bogart <klb9030@g.rit.edu>
2023-08-31 12:51:39 -0400

    Completed PE1 -  Hello World, builds successfully and fully commented

---
+++ Bogart_PE1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bogart_PE1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // test to see if a variable declaration prints the same way as plain text did
+            string hello = "Hello World!";
+            // tests another string variable to make sure they work the same
+            string myName = "Katie Bogart";
+            // test to see if a non-string will print the same way it did in Java
+            double number = 6.5;
+
+            // all prints succeed, variables print the same way as Java
+            Console.WriteLine(hello);
+            Console.WriteLine(myName);
+            Console.WriteLine(number);
+            
+
+            // both prints succeed, C# can print expressions just like Java
+            // simple expression prints after the expression is completed
+            Console.WriteLine(7 + 7);
+            // more complicated math expression follows order of operations, prints when completed
+            Console.WriteLine(6 * 7 + 15 / 2);
+
+
+            // test if explicit casting works the same way as Java
+            int num = (int)number;
+            // test if implicit casting works the same way as Java
+            double sameNum = num;
+
+            // both prints succeed, casting is the same
+            Console.WriteLine(num);
+            Console.WriteLine(sameNum);
+
+
+            // tests to see if an if/else statement works the same as in Java
+            if (num == sameNum)
+            {
+                // if this prints, int and double can be considered equal in certain circumstances
+                Console.WriteLine(num + " and " +  sameNum + " are the same!");
+            }
+            else
+            {
+                // if this prints, int and double are not equal in any circumstancec
+                Console.WriteLine(num + " and " + sameNum + " are not the same.");
+            }
+
+            // this should print 0-5 in sequence
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            double d = 0;
+
+            while (d < number)
+            {
+                Console.WriteLine(d);
+                d += 0.5;
+            }
+        }
+    }
+}
---
+++ FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,116 @@
+﻿using ColorsAndNumbers.Colors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorsAndNumbers
+{
+    namespace Colors
+    {
+        // a custom list of colors can now be access
+        enum EColors
+        {
+            red,
+            orange,
+            yellow,
+            green,
+            blue,
+            indigo,
+            violet
+        }
+    }
+
+    namespace Numbers
+    {
+        enum ENumbers
+        {
+            one,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven
+        }
+    }
+}
+
+namespace FavoriteColorAndNumber
+{
+    // perfectly fine as a using statement
+    using ColorsAndNumbers.Colors;
+    // makes the namespace more explicit and easier to track in the code
+    using ColorsAlias = ColorsAndNumbers.Colors;
+
+    // for now, every class should have a static keyword
+    static internal class Program
+    {
+        // methods consist of the static keyword a return type, a method name, and a list of paramaters
+        static void WriteMyVar(ref int nParameter)
+        {
+            Console.WriteLine(nParameter);
+            
+            // without "ref," it's a copy of the original data, not pointing to it (looking through a mirror)
+            nParameter = 42;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            // doesn't need a namespace prefix because using statement is in this namespace
+            ColorsAlias.EColors eColors;
+
+            // uses the system namespace
+            Console.WriteLine("");
+
+            int myInt = 0;
+            int anotherInt = 2;
+            string sFavColor = "";
+            string sFavNumber = "";
+            bool bValid = false;
+            int nFavNumber = 0;
+
+            anotherInt = myInt;
+
+            // ref keyword makes the method point to the original data (looking directly at it)
+            WriteMyVar(ref myInt);
+
+
+
+            // APPLICATION
+
+            // prompt the user for their favorite color
+            Console.Write("Hello! please enter your favorite color: ");
+
+            // read the favorite color provided and store it in a variable
+            sFavColor = Console.ReadLine();
+
+            // prompt the user for their favorite number
+            Console.Write("Now please enter your favorite number: ");
+
+            // read the favorite number provided and store it in a variable
+            sFavNumber = Console.ReadLine();
+            while(bValid == false)
+            {
+                try
+                {
+                    nFavNumber = Convert.ToInt32(sFavNumber);
+                    bValid = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer.");
+                }
+            }
+
+            // a loop that outputs their favorite color their favorite number of times
+            for (int i = 0; i < nFavNumber; i++)
+            {
+                Console.WriteLine(sFavColor);
+            }
+        }
+    }
+}
