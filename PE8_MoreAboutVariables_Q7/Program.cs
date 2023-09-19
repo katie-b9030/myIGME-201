8d629ce1b8f893fb1759a16d83b8aa460fd17e84
Katie Bogart <klb9030@g.rit.edu>
2023-09-18 21:56:08 -0400

    Code is commented and works properly

--- PE8_MoreAboutVariables_Q7/Program.cs
+++ PE8_MoreAboutVariables_Q7/Program.cs
@@ -19,6 +19,24 @@ namespace PE8_MoreAboutVariables_Q7
          */
         static void Main(string[] args)
         {
+            string initialWord;
+            char[] letters;
+
+            // accept a string from the user
+            Console.WriteLine("Please enter a word: ");
+            initialWord = Console.ReadLine();
+
+            // split that string into letters
+            letters = initialWord.ToCharArray();
+
+            Console.Write("Here is your reversed word: ");
+
+            // print each letter of the word in reverse order
+            for (int i = letters.Length- 1; i >= 0; i--)
+            {
+                Console.Write(letters[i]);
+            }
+            Console.WriteLine();
 
         }
     }
