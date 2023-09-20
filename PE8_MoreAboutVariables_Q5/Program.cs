e9d5e58e7cc8a709cb1b376bc323d9cbc3f98ff9
Katie Bogart <klb9030@g.rit.edu>
2023-09-20 12:46:13 -0400

    code is decently commented and program appears to work properly

--- PE8_MoreAboutVariables_Q5/Program.cs
+++ PE8_MoreAboutVariables_Q5/Program.cs
@@ -22,7 +22,7 @@ namespace PE8_MoreAboutVariables_Q5
         static void Main(string[] args)
         {
             // create an array to store the values
-            double[,] xyzVals = new double[3, 21*31];
+            double[,,] xyzVals = new double[3, 1, 21*31];
             int xCols = 0;
             int yCols = 0;
             int zCols = 0;
@@ -30,14 +30,14 @@ namespace PE8_MoreAboutVariables_Q5
             // input the values of x
             for (double x = -1; x <= 1; x += 0.1)
             {
-                xyzVals[0, xCols] = x;
+                xyzVals[0, 0, xCols] = x;
                 xCols++;
             }
 
             // input the values of y
             for (double y = 1; y <= 4; y += 0.1)
             {
-                xyzVals[1, yCols] = y;
+                xyzVals[1, 0, yCols] = y;
                 yCols++;
             }
 
@@ -46,18 +46,32 @@ namespace PE8_MoreAboutVariables_Q5
             {
                 for (int y = 0; y < yCols; y++)
                 {
-                    xyzVals[2, zCols] = (3*Math.Pow(xyzVals[1,y], 2) + 2*(xyzVals[0, x]) - 1);
+                    xyzVals[2, 0, zCols] = (3*Math.Pow(xyzVals[1, 0, y], 2) + 2*(xyzVals[0, 0, x]) - 1);
+                    zCols++;
                 }
             }
 
+            Console.WriteLine("Here is your array: ");
             // print the array
+            // each row
             for (int i = 0; i < 3; i++)
             {
+                Console.Write("[ ");
+                // each col
                 for (int j = 0; j < zCols; j++)
                 {
-                    Console.WriteLine("Here is your array: ");
-                    Console.WriteLine(xyzVals[i, j]);
+                    // if the value isn't empty, print it
+                    if ((i < 2) && (xyzVals[i, 0, j] != 0))
+                    {
+                           Console.Write(xyzVals[i, 0, j] + " ");
+                    }
+                    else if (i == 2)
+                    {
+                        Console.Write(xyzVals[i, 0, j] + " ");
+                    }
+                    
                 }
+                Console.WriteLine("]");
             }
         }
     }
