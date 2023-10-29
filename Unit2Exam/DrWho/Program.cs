6ae0d3bec149da0e33b86742bf60022be2d5a090
Katie Bogart <klb9030@g.rit.edu>
2023-10-29 17:40:42 -0400

    Finished Dr. Who through question 7 on the exam

---
+++ Unit2Exam/DrWho/Program.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrWho
+{
+    /* Class: Phone
+     * Author: Katie Bogart
+     * Purpose: 
+     * Restrictions: None
+     */
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+
+        public string PhoneNumber
+        {
+            get 
+            {
+                return phoneNumber;
+            }
+            set 
+            { 
+                phoneNumber = value; 
+            }
+        }
+
+        public abstract void Connect();
+        public abstract void Disconnect();
+    }
+
+    /* Interface: PhoneInterface
+     * Author: Katie Bogart
+     * Purpose: 
+     * Restrictions: None
+     */
+    public interface PhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+    }
+
+    /* Class: RotaryPhone
+     * Author: Katie Bogart
+     * Purpose: 
+     * Restrictions: None
+     */
+    public class RotaryPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+            
+        }
+    }
+
+    /* Class: PushButtonPhone
+     * Author: Katie Bogart
+     * Purpose: 
+     * Restrictions: None
+     */
+    public class PushButtonPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    /* Class: Tardis
+     * Author: Katie Bogart
+     * Purpose: 
+     * Restrictions: None
+     */
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho
+        {
+            get
+            {
+                return whichDrWho;
+            }
+        }
+
+        public string FemaleSideKick
+        {
+            get
+            {
+                return femaleSideKick;
+            }
+        }
+
+        public void TimeTravel()
+        {
+
+        }
+
+        public virtual bool Equals(Tardis other)
+        {
+            if (this.whichDrWho == other.whichDrWho)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <(Tardis left, Tardis right)
+        {
+            if ((right.whichDrWho == 10) || (left.whichDrWho < right.whichDrWho))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator >(Tardis left, Tardis right)
+        {
+            if ((left.whichDrWho == 10) || (left.whichDrWho > right.whichDrWho))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <=(Tardis left, Tardis right)
+        {
+            if ((left.whichDrWho <= right.whichDrWho))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator >=(Tardis left, Tardis right)
+        {
+            if ((left.whichDrWho >= right.whichDrWho))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+    /* Class: PhoneBooth
+     * Author: Katie Bogart
+     * Purpose: 
+     * Restrictions: None
+     */
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor()
+        {
+
+        }
+
+        public void CloseDoor()
+        {
+
+        }
+    }
+
+    /* Class: Program
+     * Author: Katie Bogart
+     * Purpose: 
+     * Restrictions: None
+     */
+    internal class Program
+    {
+        static void UsePhone(object obj)
+        {
+            try
+            {
+                PhoneInterface phone = (PhoneInterface)obj;
+                phone.MakeCall();
+                phone.HangUp();
+
+                if (obj.GetType() == typeof(PhoneBooth))
+                {
+                    PhoneBooth booth = (PhoneBooth)obj;
+                    booth.OpenDoor();
+                }
+
+                else if (obj.GetType() == typeof(Tardis))
+                {
+                    Tardis booth = (Tardis)obj;
+                    booth.TimeTravel();
+                }
+            }
+            catch 
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Tardis tardis = new Tardis();
+            PhoneBooth phoneBooth = new PhoneBooth();
+
+            UsePhone(tardis);
+            UsePhone(phoneBooth);
+        }
+    }
+}
---
+++ Unit2Exam/Unit2ExamTests/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit2ExamTests
+{
+    public sealed class Circus
+    {
+        public string name;
+    }
+
+    public class MyClass
+    {
+        public int myInt;
+
+        public MyClass(int nVal)
+        {
+            this.myInt += nVal;
+        }
+    }
+
+    public class MyDerivedClass : MyClass
+    {
+        public MyDerivedClass(int nVal) : base(nVal)
+        {
+            this.myInt = (this.myInt + 2) * 4;
+        }
+    }
+
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            (double, double, double) wxyVals;
+            SortedList<(double, double, double), double> equationVals = new SortedList<(double, double, double), double>();
+            double dW, dX, dY, dZ;
+            for (dW = -2; dW <= 0; dW += 0.2)
+            {
+                dW = Math.Round(dW, 1);
+                for (dX = -1; dX <= 1; dX += 0.1)
+                {
+                    dX = Math.Round(dX, 1);
+                    for (dY = 0; dY <= 4; dY += 0.1)
+                    {
+                        dY = Math.Round(dY, 1);
+                        wxyVals = (dW, dX, dY);
+
+                        dZ = 4 * dY * dY * dY + 2 * dX * dX - 8 * dW + 7;
+                        dZ = Math.Round(dZ, 3);
+                        equationVals.Add(wxyVals, dZ);
+
+                    }
+                }
+            }
+        }
+    }
+
+}
