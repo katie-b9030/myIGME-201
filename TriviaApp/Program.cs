e0e7058dc20b8bd520f6bfc7798ba67a34aac026
Katie Bogart <klb9030@g.rit.edu>
2023-11-21 19:45:57 -0500

    Trivia completed

--- TriviaApp/Program.cs
+++ TriviaApp/Program.cs
@@ -15,6 +15,7 @@ namespace TriviaApp
         public string category;
         public string type;
         public string difficulty;
+        public string question;
         public string correct_answer;
         public List<string> incorrect_answers;
     }
@@ -22,13 +23,36 @@ namespace TriviaApp
     class Trivia
     {
         public int response_code;
-        public List<TriviaResult> trivia;
+        public List<TriviaResult> results;
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            string url = null;
+            string s = null;
+
+            HttpWebRequest request;
+            HttpWebResponse response;
+            StreamReader reader;
+
+            url = "https://opentdb.com/api.php?amount=1&type=multiple";
+
+            request = (HttpWebRequest)WebRequest.Create(url);
+            response = (HttpWebResponse)request.GetResponse();
+            reader = new StreamReader(response.GetResponseStream());
+            s = reader.ReadToEnd();
+            reader.Close();
+
+            Trivia trivia = JsonConvert.DeserializeObject<Trivia>(s);
+
+            trivia.results[0].question = HttpUtility.HtmlDecode(trivia.results[0].question);
+            trivia.results[0].correct_answer = HttpUtility.HtmlDecode(trivia.results[0].correct_answer);
+            for (int i = 0; i < trivia.results[0].incorrect_answers.Count; ++i)
+            {
+                trivia.results[0].incorrect_answers[i] = HttpUtility.HtmlDecode(trivia.results[0].incorrect_answers[i]);
+            }
         }
     }
 }
