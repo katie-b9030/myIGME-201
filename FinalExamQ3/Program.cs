f715976d37447fc34a6a3c4115830f9880c1c595
Katie Bogart <klb9030@g.rit.edu>
2023-12-10 17:07:47 -0500

    completed all 7 exercises

---
+++ FinalExamQ3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExamQ3
+{
+    enum EVertices
+    {
+        red,
+        orange,
+        yellow,
+        green,
+        blue,
+        indigo,
+        violet,
+        grey
+    }
+
+    /* Class: Program
+     * Author: Katie Bogart
+     * Purpose: 
+     * Restrictions: None
+     */
+    internal class Program
+    {
+        static int[,] digraphWeights = new int[,]
+        {        /*  red  orange  yellow   green   blue   indigo  violet  grey */
+            /*red*/{ -1,    -1,     -1,     -1,     -1,      1,     -1,      5 },
+         /*orange*/{ -1,    -1,     -1,     -1,     -1,     -1,      1,     -1 },
+         /*yellow*/{ -1,    -1,     -1,      6,     -1,     -1,     -1,     -1 },
+          /*green*/{ -1,    -1,     -1,     -1,     -1,     -1,     -1,     -1 },
+           /*blue*/{ -1,    -1,     -1,     -1,     -1,      1,     -1,      0 },
+         /*indigo*/{ -1,    -1,      8,     -1,      1,     -1,     -1,     -1 },
+         /*violet*/{ -1,    -1,      1,     -1,     -1,     -1,     -1,     -1 },
+           /*grey*/{ -1,     1,     -1,     -1,      0,     -1,     -1,     -1 },
+        };
+
+        /* Method: Main
+         * Purpose: 
+         * Restrictions: None
+         */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
